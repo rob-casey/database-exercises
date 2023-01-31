47e85a5ac2a27d6c001dc13b03782e17a1a81d25
Rob Casey <rob.p.casey1@gmail.com>
2023-01-31 12:55:51 -0600

    Saving Progress through 7

--- join_exercises.sql
+++ join_exercises.sql
@@ -45,20 +45,22 @@ DESCRIBE titles;
 -- title
 -- from_date
 -- to_date
+DESCRIBE dept_emp;
+-- emp_no
+-- dept_no
+-- from_date
+-- to_date
+DESCRIBE salaries;
+-- emp_no
+-- salary
+-- from_table
+-- to_table
+
 
 
 SELECT * FROM departments;
 SELECT * FROM dept_manager;
 SELECT * FROM employees;
-
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
-FROM employees AS e
-JOIN dept_manager AS dm
-ON e.emp_no = dm.emp_no
-JOIN department as d
-ON d.dept_no = dm.dept_no
-WHERE dm.to_date = '9999%'
-ORDER BY d.dept_name;
 
 SELECT departments.dept_name as Workspace, CONCAT(employees.first_name, ' ', employees.last_name) as Full_name
 FROM dept_manager DM
@@ -68,17 +70,53 @@ WHERE DM.to_date LIKE '9999%'
 ORDER BY departments.dept_name;
 
 
--- JOIN department as d
--- ON d.dept_no = dm.dept_no;
+-- Find the name of all departments currently managed by women.
 
--- JOIN departments AS d
--- ON (d.dept_no = dm.dept_no)
--- WHERE dept_manager.to_date = '9999%';
-
-SELECT departments.dept_name, concat(employees.first_name," ",employees.last_name)
+SELECT departments.dept_name as Workspace, CONCAT(employees.first_name, ' ', employees.last_name) as Manager, Gender
 FROM dept_manager DM
+JOIN departments ON DM.dept_no = departments.dept_no
+JOIN employees ON DM.emp_no = employees.emp_no
+WHERE DM.to_date LIKE '9999%' and employees.gender = 'F';
+
+-- Find the current titles of employees currently working in the customer service department
+
+SELECT departments.dept_name as Service, CONCAT(employees.first_name, ' ', employees.last_name) as Employee, employees.emp_no as Employ_No, titles.title 
+FROM dept_emp DE
+JOIN departments ON DE.dept_no = departments.dept_no
+JOIN employees on DE.emp_no = employees.emp_no
+JOIN titles on DE.emp_no = titles.emp_no
+WHERE DE.to_date LIKE '9999%' and titles.to_date LIKE '9999%';
+
+-- Find the current Salary of all current managers.
+
+SELECT departments.dept_name as 'Department Name', CONCAT(employees.first_name, ' ', employees.last_name) as Name, salaries.salary as Salary
+FROM dept_manager DM
+JOIN employees ON DM.emp_no = employees.emp_no
+JOIN salaries ON DM.emp_no = salaries.emp_no
 JOIN departments on DM.dept_no = departments.dept_no
-JOIN employees on DM.emp_no = employees.emp_no
-WHERE DM.to_date = '9999-01-01'
-ORDER BY departments.dept_name;
+WHERE DM.to_date and salaries.to_date LIKE '9999%';
+
+-- Find the number of current employees in each department.
+
+SELECT departments.dept_no, departments.dept_name as 'Department Name', COUNT(*)
+FROM departments
+JOIN dept_emp ON departments.dept_no = dept_emp.dept_no
+WHERE dept_emp.to_date LIKE '9999%'
+GROUP BY departments.dept_no
+ORDER BY departments.dept_no DESC;
+
+-- Which departments has the highest average salary? hint: use current, not historic information
+
+SELECT departments.dept_name as 'Department Name', round(avg(salaries.salary)) as 'Average Salary'
+FROM salaries
+JOIN dept_emp ON salaries.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE dept_emp.to_date LIKE '9999%' and salaries.to_date LIKE '9999%' and departments.dept_name = 'Sales'
+GROUP BY departments.dept_name;
+
+-- Who is the highest paid employee in the Marketing Department?
+
+
+
+
 
