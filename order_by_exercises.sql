c683f5d8c087d22391496df9f69e755edd2502ad
Rob Casey <rob.p.casey1@gmail.com>
2023-02-01 14:32:20 -0600

    Final Push for Module Review

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,98 +1,101 @@
+
+
+-- (1) Create a new file named order_by_exercises.sql and copy in the contents of your exercise from the previous lesson.
+
+-- (2a) Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by first name. 
+
 USE employees;
+SHOW TABLES;
 
-SELECT * FROM employees 
-WHERE (first_name = 'Irena') 
-OR  (first_name = 'Vidya') 
-OR (first_name = 'Maya')
-ORDER BY first_name LIMIT 1;
+SELECT * FROM employees WHERE
+first_name = 'Irena' OR
+first_name = 'Vidya' OR
+first_name = 'Maya' ORDER BY
+first_name;
 
--- What was the first and last name in the first row of the results? Irena Reutenauer
+-- In your comments, answer: 
+-- (2b) What was the first and last name in the first row of the results? 
+-- Irena Reutenauer
 
-SELECT * FROM employees 
-WHERE (first_name = 'Irena') 
-OR  (first_name = 'Vidya') 
-OR (first_name = 'Maya')
-ORDER BY first_name DESC LIMIT 1;
+-- (2c) What was the first and last name of the last person in the table?
+-- Vidya Awdeh
 
--- What was the first and last name of the last person in the table? Vidya Awdeh
+-- (3a) Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by first name and then last name. 
 
-SELECT * FROM employees 
-WHERE (first_name = 'Irena') 
-OR  (first_name = 'Vidya') 
-OR (first_name = 'Maya')
-ORDER BY first_name, last_name
-LIMIT 1;
+SELECT * FROM employees WHERE
+first_name = 'Irena' OR
+first_name = 'Vidya' OR
+first_name = 'Maya' ORDER BY
+first_name, last_name;
 
--- (Inst) Refers to IN vs. OR, by following the INstructions.
--- SELECT * FROM employees 
--- WHERE first_name IN ('Irena', 'Vidya', 'Maya')
--- ORDER BY first_name, last_name
--- LIMIT 1;
+-- In your comments, answer: 
+-- (3b) What was the first and last name in the first row of the results? 
+-- Irena Acton
 
--- What was the first and last name in the first row of the results? Irena Acton
+-- (3c) What was the first and last name of the last person in the table?
+-- Vidya Zweizig
 
-SELECT * FROM employees 
-WHERE (first_name = 'Irena') 
-OR  (first_name = 'Vidya') 
-OR (first_name = 'Maya')
-ORDER BY first_name DESC , last_name DESC
-LIMIT 1;
+-- (4a) Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by last name and then first name. 
 
--- What was the first and last name of the last person in the table? Vidya Zweisig
+SELECT * FROM employees WHERE
+first_name = 'Irena' OR
+first_name = 'Vidya' OR 
+first_name = 'Maya' ORDER BY
+last_name, first_name;
 
-SELECT * FROM employees
-WHERE (first_name = 'Irena') 
-OR  (first_name = 'Vidya') 
-OR (first_name = 'Maya')
-ORDER BY last_name, first_name;
+-- In your comments, answer: 
+-- (4b) What was the first and last name in the first row of the results? 
+-- Irena Acton
 
--- What was the first and last name in the first row of the results? Irena Acton
--- What was the first and last name of the last person in the table? Maya Zyda
+-- (4c) What was the first and last name of the last person in the table?
+-- Maya Zyda
 
-SELECT * FROM employees
-WHERE last_name LIKE 'E%e'
-ORDER BY emp_no
-Limit 1;
+-- (5) Write a query to to find all employees whose last name starts and ends with 'E'. 
+-- 	   Sort the results by their employee number. 
+--     Enter a comment with the number of employees returned, 
+--     the first employee number and their first and last name, 
+--     and the last employee number with their first and last name.
 
--- Enter a comment with the number of employees returned: 899 rows returned
--- the first employee number and their first and last name: 10021, Ramzi Erde
--- (INST) Can use Limit 1, LIMIT 1 DESC
+SELECT * FROM employees WHERE
+last_name LIKE 'E%e' ORDER BY
+emp_no;
 
-SELECT * FROM employees
-WHERE last_name LIKE 'E%e'
-ORDER BY emp_no DESC
-Limit 1;
+/* Answer
+899 Employees returned
+10021, Ramzi Erde is the first employee returned
+499648, Tadahiro Erde is the last employee returned 
+*/
 
--- and the last employee number with their first and last name: 499648, Tadihiro Erde
+-- (6) Write a query to to find all employees whose last name starts and ends with 'E'. 
+-- 	   Sort the results by their hire date, so that the newest employees are listed first. 
+--     Enter a comment with the number of employees returned, 
+--     the name of the newest employee, 
+--     and the name of the oldest employee.
 
-SELECT * FROM employees
-WHERE last_name LIKE 'E%e'
-ORDER BY hire_date DESC;
+SELECT * FROM employees WHERE
+last_name LIKE 'E%e' ORDER BY
+hire_date DESC;
 
--- Enter a comment with the number of employees returned: 899 rows returned
--- the name of the newest employee: Teiji Eldridge
+/* Answer
+899 Employees returned
+Teiji Eldridge is the newest employee
+Sergi Erde is the oldest employee
+*/
 
-SELECT * FROM employees
-WHERE last_name LIKE 'E%e'
-ORDER BY hire_date, birth_date;
+-- (7) Find all employees hired in the 90s and born on Christmas. 
+--     Sort the results so that the oldest employee who was hired last is the first result. 
+--     Enter a comment with the number of employees returned, 
+--     the name of the oldest employee who was hired last, 
+--     and the name of the youngest employee who was hired first.
 
--- and the name of the oldest employee: Serge Erde
+SELECT * FROM employees WHERE
+hire_date LIKE '199%' AND
+birth_date LIKE '%12-25' ORDER BY
+birth_date DESC,
+hire_date ASC;
 
-SELECT * FROM employees
-WHERE hire_date LIKE '199%' 
-AND birth_date LIKE '%12-25'
-ORDER BY birth_date, hire_date DESC;
-
--- Enter a comment with the number of employees returned: 362 rows returned
--- the name of the oldest employee who was hired last: Khun Bernini
-
-SELECT * FROM employees
-WHERE hire_date LIKE '199%' 
-AND birth_date LIKE '%12-25'
-ORDER BY birth_date DESC, hire_date;
-
--- and the name of the youngest employee who was hired first: Alselm Cappello
-
-
-
-
+/* Answer
+362 employees returned
+Khun Bernini is the oldest employee hired last
+Douadi Pettis is the youngest employee hired first
+*/
